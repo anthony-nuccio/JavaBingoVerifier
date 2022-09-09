7dfc47788b9ea7046bac4735de39332042bd7f38
whoremonger69 <87153552+whoremonger69@users.noreply.github.com>
2022-09-08 21:25:30 -0400

    Created Java Class

---
+++ CardVerifier.java
@@ -0,0 +1,9 @@
+/* 
+ * Name: Anthony Nuccio
+ * Date: Suday, September 11th, 2022
+ * Project: Bingo Card Verifier (Java)
+ * Class: CSC 1800 | Organization of Programming Languages
+ */
+
+public class CardVerifier {
+}

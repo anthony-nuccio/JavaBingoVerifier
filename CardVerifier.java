67d3733677abf011a8e3d55ff9d70c5d511a6ea9
whoremonger69 <87153552+whoremonger69@users.noreply.github.com>
2022-09-10 12:03:32 -0400

    Deleted Old File

--- CardVerifier.java
+++
@@ -1,9 +0,0 @@
-/* 
- * Name: Anthony Nuccio
- * Date: Suday, September 11th, 2022
- * Project: Bingo Card Verifier (Java)
- * Class: CSC 1800 | Organization of Programming Languages
- */
-
-public class CardVerifier {
-}
